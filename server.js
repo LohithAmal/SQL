8885804d9559702ddc3caa57caacc1273071d8b0
lohith <lohith05amal@gmail.com>
2021-09-12 21:19:33 -0400

    express to sql query added

--- server.js
+++ server.js
@@ -3,13 +3,35 @@ const express = require('express');
 const PORT =process.env.PORT || 3001;
 
 const app = express();
-
+const mysql = require('mysql2');
 
 // express middleware
 
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+
+// code to connect database here
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // mysql username
+    user: 'root',
+    // mysql password
+    password: 'Quality8055]',
+    database: 'election'
+  },
+  console.log('connected to election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+
+
 // 404 error
 app.use((req,res)=>{
   res.status(404).end();
