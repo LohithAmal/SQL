0afa09ca135d4d32b00e5476818ad6557f6efc02
lohith <lohith05amal@gmail.com>
2021-09-18 12:31:14 -0400

    delete a candidate

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const mysql = require('mysql2');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
+//  app.get('/',(req,res) =>{
+//    res.json({ message: 'Hello world'});
+//  });
 // code to connect database here
 
 const db = mysql.createConnection(
@@ -26,13 +28,44 @@ const db = mysql.createConnection(
   console.log('connected to election database')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id=1`, (err,row) =>{
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+
+// ====================
+// Delete a candidate
+// =====================
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+// ============================
+// >>>>>>>>>>>>>create a candidate
+// ============================
+const sql = `INSERT INTO candidates(id,first_name,last_name,industry_connected)
+VALUES (?,?,?,?)`;
+const params = [1,'Ronald','Firbank',1];
+db.query(sql,params,(err,result) =>{
+  if(err){
+    console.log(err);
+  }
+  console.log(result);
 });
 
 
-
+// =================
 // 404 error
+// ================
 app.use((req,res)=>{
   res.status(404).end();
 })
